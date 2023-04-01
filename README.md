ec75d6f88d8672826931b0d52252046c4be02796
kazi-kabir <kazi.s221999@gmail.com>
2023-04-01 12:49:06 +1100

    working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hacker-news-clone
+
+what do i want to achieve?
+- create a new div that holds the post summary
+- make it a child of the contentarea div
--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-var div = document.createElement("div");
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-div.style.color = "white";
-div.innerHTML = "Hello";
+// Creating a div element
+var divElement = document.createElement("Div");
+divElement.id = "divID";
 
-document.body.appendChild(div);
+
+// Styling it
+divElement.style.textAlign = "center";
+divElement.style.fontWeight = "bold";
+divElement.style.fontSize = "smaller";
+divElement.style.paddingTop = "15px";
+
+// Adding a paragraph to it
+var paragraph = document.createElement("P");
+var text = document.createTextNode("Another paragraph, yay! This one will be styled different from the rest since we styled the DIV we specifically created.");
+paragraph.appendChild(text);
+divElement.appendChild(paragraph);
+
+// Adding a button, cause why not!
+var button = document.createElement("Button");
+var textForButton = document.createTextNode("Release the alert");
+button.appendChild(textForButton);
+button.addEventListener("click", function(){
+    alert("Hi!");
+});
+divElement.appendChild(button);
+
+// Appending the div element to body
+//document.getElementsByClassName("contentarea")[0].appendChild
+
+let fembar = document.getElementsByClassName("contentarea");
+
+console.log(fembar)
