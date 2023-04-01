72f96151f8272234089dadad62200a015d78f936
kazi-kabir <kazi.s221999@gmail.com>
2023-04-01 14:02:15 +1100

    move into func

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // Creating a div element
-var divElement = document.createElement("Div");
-divElement.id = "divID";
 
 
-// Styling it
-divElement.style.textAlign = "center";
-divElement.style.fontWeight = "bold";
-divElement.style.fontSize = "smaller";
-divElement.style.paddingTop = "15px";
+function createDivs(num) {
+    var divElement = document.createElement("Div");
+    divElement.id = "divID";
 
-// Adding a paragraph to it
-var paragraph = document.createElement("P");
-var text = document.createTextNode("Another paragraph, yay! This one will be styled different from the rest since we styled the DIV we specifically created.");
-paragraph.appendChild(text);
-divElement.appendChild(paragraph);
 
-const contentArea = document.getElementsByClassName('contentarea');
-console.log(contentArea);
-contentArea[0].appendChild(divElement);
+    // Styling it
+    divElement.style.textAlign = "center";
+    divElement.style.fontWeight = "bold";
+    divElement.style.fontSize = "smaller";
+    divElement.style.paddingTop = "15px";
+
+    // Adding a paragraph to it
+    var paragraph = document.createElement("P");
+    var text = document.createTextNode("Another paragraph, yay! This one will be styled different from the rest since we styled the DIV we specifically created.");
+    paragraph.appendChild(text);
+    divElement.appendChild(paragraph);
+
+    const contentArea = document.getElementsByClassName('contentarea');
+    console.log(contentArea);
+    contentArea[0].appendChild(divElement);
+}
